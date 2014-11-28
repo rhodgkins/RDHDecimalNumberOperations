c14b2baffd55c51774bacd1dd578bc63dafbb828
Rich Hodgkins <rhodgkins@gmail.com>
2014-11-28 20:31:08 +0000

    Initial project commit

---
+++ RDHDecimalNumberOperations/RDHDecimalNumberOperations.h
@@ -0,0 +1,19 @@
+//
+//  RDHDecimalNumberOperations.h
+//  RDHDecimalNumberOperations
+//
+//  Created by Richard Hodgkins on 28/11/2014.
+//  Copyright (c) 2014 Rich H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RDHDecimalNumberOperations.
+FOUNDATION_EXPORT double RDHDecimalNumberOperationsVersionNumber;
+
+//! Project version string for RDHDecimalNumberOperations.
+FOUNDATION_EXPORT const unsigned char RDHDecimalNumberOperationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RDHDecimalNumberOperations/PublicHeader.h>
+
+
---
+++ RDHDecimalNumberOperations/RDHDecimalNumberOperations.swift
@@ -0,0 +1,9 @@
+//
+//  RDHDecimalNumberOperations.swift
+//  RDHDecimalNumberOperations
+//
+//  Created by Richard Hodgkins on 28/11/2014.
+//  Copyright (c) 2014 Rich H. All rights reserved.
+//
+
+import Foundation
